1b0873128319387a30ecd5aa33ca2c34227f0b1b
elberthcorniell <elberthcorniell@gmail.com>
2024-05-28 18:02:33 -0400

    Update index.tsx

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -12,45 +12,52 @@ export default async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
 
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
-        <Suspense fallback={null}>
-          <MobileMenu menu={menu} />
-        </Suspense>
-      </div>
-      <div className="flex w-full items-center">
-        <div className="hidden justify-center md:flex md:w-1/3"></div>
-        <div className="flex w-full md:w-1/3">
-          <Link
-            href="/"
-            className="mr-2 flex w-full items-center justify-center md:!mx-auto md:w-auto lg:mr-6"
-          >
-            <Logo height={45} />
-          </Link>
-          {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => (
-                <li key={item.title}>
-                  <Link
-                    href={item.path}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline "
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : null}
-        </div>
-        <div className="flex justify-end md:w-1/3">
-          <div className=" mr-2 hidden h-full md:block">
-            <FloatingSearch />
-          </div>
-          <Suspense fallback={<OpenCart />}>
-            <Cart />
+    <>
+      <nav className="relative flex items-center justify-between p-4 lg:px-6">
+        <div className="block flex-none md:hidden">
+          <Suspense fallback={null}>
+            <MobileMenu menu={menu} />
           </Suspense>
         </div>
+        <div className="flex w-full items-center">
+          <div className="hidden justify-center md:flex md:w-1/3"></div>
+          <div className="flex w-full md:w-1/3">
+            <Link
+              href="/"
+              className="mr-2 flex w-full items-center justify-center md:!mx-auto md:w-auto lg:mr-6"
+            >
+              <Logo height={45} />
+            </Link>
+            {menu.length ? (
+              <ul className="hidden gap-6 text-sm md:flex md:items-center">
+                {menu.map((item: Menu) => (
+                  <li key={item.title}>
+                    <Link
+                      href={item.path}
+                      className="text-neutral-500 underline-offset-4 hover:text-black hover:underline "
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+          <div className="flex justify-end md:w-1/3">
+            <div className=" mr-2 hidden h-full md:block">
+              <FloatingSearch />
+            </div>
+            <Suspense fallback={<OpenCart />}>
+              <Cart />
+            </Suspense>
+          </div>
+        </div>
+      </nav>
+      <div className=" w-screen bg-black px-4 py-2 ">
+        <p className=" w-full text-center text-sm font-bold uppercase text-white">
+          Envío a toda la República Dominicana 🇩🇴
+        </p>
       </div>
-    </nav>
+    </>
   );
 }
